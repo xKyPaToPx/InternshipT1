df6ad8347c674252da2fe5946afa11aac294efe0
Yakov Burtik <100014633+FatalOptimist@users.noreply.github.com>
2024-04-16 13:24:12 +0300

    update gitignore

---
+++ DataAccess/Program.cs
@@ -0,0 +1,13 @@
+﻿// Configure database
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using pisLab1;
+
+var dbBuilder = new ConfigurationBuilder();
+dbBuilder.SetBasePath(Directory.GetCurrentDirectory());
+dbBuilder.AddJsonFile("appsettings.json");
+var config = dbBuilder.Build();
+var connectionString = config.GetConnectionString("DefaultConnection");
+var optionsBuilder = new DbContextOptionsBuilder<SpyContext.ApplicationContext>();
+SpyContext.Options = optionsBuilder.UseSqlServer(connectionString).Options;
